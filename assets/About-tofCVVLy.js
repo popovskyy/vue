efe3eb1132650459981c1fa469881f89f5facb1e
popovskiy <roman.p@overdose.digital>
2023-12-18 11:52:39 +0200

    Updates

--- assets/About-tofCVVLy.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as r,t as s}from"./index-OZkxt88M.js";const n={name:"About",data(){return{msg:"hello world2"}}};function a(c,l,p,_,t,u){return e(),r("h1",null,s(t.msg),1)}const f=o(n,[["render",a]]);export{f as default};
