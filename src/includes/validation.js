1705d5eec1e6b9f674dd242dfa53b269f738510a
popovskiy <roman.p@overdose.digital>
2023-12-19 13:29:11 +0200

    Customization error message, added validation for login form, created 2 separete components for each form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,4 +1,4 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate';
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure } from 'vee-validate';
 import {
     required,
     min,
@@ -18,13 +18,42 @@ export default {
         app.component('ErrorMessage', ErrorMessage);
 
         defineRule('required', required);
+        defineRule('tos', required);
         defineRule('min', min);
         defineRule('max', max);
         defineRule('alpha_spaces', alphaSpaces);
         defineRule('email', email);
         defineRule('min_value', minVal);
         defineRule('max_value', maxVal);
-        defineRule('confirmed', confirmed);
+        defineRule('password_mismatch', confirmed);
         defineRule('not_one_of', not_one_of);
+        defineRule('country_excluded', not_one_of);
+
+        configure({
+            generateMessage: (ctx) => {
+                const messages = {
+                    required: `The field ${ctx.field} is required.`,
+                    min: `The field ${ctx.field} is too short.`,
+                    max: `The field ${ctx.field} is too short.`,
+                    alpha_spaces: `The field ${ctx.field} may only contain alphabetical chars`,
+                    email: `The field ${ctx.field} must be a valid email.`,
+                    min_value: `The field ${ctx.field} is too low.`,
+                    max_value: `The field ${ctx.field} is too high.`,
+                    password_mismatch: `The password don't match.`,
+                    not_one_of: `You are not arrowed to use this value for the field ${ctx.field}.`,
+                    country_excluded: `Due to restrictions, we do not accept users from this location.`,
+                    tos: `You must accept the Terms of Service.`
+                };
+
+                const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid.`;
+
+                return message;
+            },
+
+            validateOnBlur: true, // it's default value  from vee-validation lib
+            validateOnChange: true, // it's default value  from vee-validation lib
+            validateOnInput: false, // it's default value  from vee-validation lib
+            validateOnModelUpdate: true // it's default value  from vee-validation lib
+        });
     }
 };
